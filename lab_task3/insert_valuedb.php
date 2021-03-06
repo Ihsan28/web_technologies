37463ae93a07b3138ecd2c8923c761edee98d205
Ihsan28 <58226483+Ihsan28@users.noreply.github.com>
2021-03-07 02:21:44 +0600

    simple update

--- lab_task3/insert_valuedb.php
+++ lab_task3/insert_valuedb.php
@@ -8,6 +8,7 @@ $password = "";
 $dbname = "mydb";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);}
+return $conn;
 }
 
 // $user = " ihsan ";
